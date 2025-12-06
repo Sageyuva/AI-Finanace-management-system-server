d768f2ad54358e064bb78ba080cff4d7242368f2
yuvaraj <yuvarajdhammure@gmail.com>
2025-12-06 17:56:44 +0530

    setup entry point and as well as configured app

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const cors = require("cors")
+
+
+//initialize app
+const app = express()
+
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+//baseUrl
+const baseurl = process.env.BASE_URL
+
+
+//routes
+app.get(baseurl, (req, res) => {
+  res.send("Hello World!");
+});
+
+
+module.exports = app
