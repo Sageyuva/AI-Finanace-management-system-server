c061090ff8df16289824f440f24d98e51df3e5e9
yuvaraj dhammure <yuvarajdhammure@gmail.com>
2026-01-09 12:09:22 +0530

    added teh token from teh cookies

--- src/Middleware/secure.js
+++ src/Middleware/secure.js
@@ -1,39 +1,38 @@
 const jwt = require("jsonwebtoken")
 const userModel = require("../Models/userModel")
-const jwtSign = async(userId) => {
+const jwtSign = async (userId) => {
     try {
-        const token = jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"1d"})
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1d" })
         return token
     } catch (error) {
         console.log(error)
         return error
     }
 }
-const protectUser = async(req,res,next) => {
-    let token;
-    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+const protectUser = async (req, res, next) => {
+    let token = req.cookies.token;
+    if (token) {
         try {
-            token = req.headers.authorization.split(" ")[1]
-            const decode = jwt.verify(token,process.env.JWT_SECRET)
-            if(decode){
+            const decode = jwt.verify(token, process.env.JWT_SECRET)
+            if (decode) {
                 const reqId = decode.userId
                 const user = await userModel.findById(reqId)
-                if(!user){
-                    return res.status(401).json({message:"Invalid token"})
+                if (!user) {
+                    return res.status(401).json({ message: "Invalid token" })
                 }
                 req.user = user
                 next()
             }
-            else{
-                return res.status(401).json({message:"Invalid token"})
-            }            
+            else {
+                return res.status(401).json({ message: "Invalid token" })
+            }
         } catch (error) {
-            console.log("Invalid token" , error)
+            console.log("Invalid token", error)
             return error
         }
     }
-    else{
-        return res.status(401).json({message:"Invalid token"})
+    else {
+        return res.status(401).json({ message: "Invalid token" })
     }
 }
-module.exports = {jwtSign,protectUser}
+module.exports = { jwtSign, protectUser }
